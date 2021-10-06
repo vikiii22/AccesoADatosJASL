a4f8e2fdb8c1dceef90fd1e07bd9a837130c3c53
JoseASL <joseviki10@gmail.com>
2021-10-06 17:02:55 +0200

    Empezando procesos

--- src/Servicios/Ejercicio5.java
+++ src/Servicios/Ejercicio5.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Scanner;
-
+//Cambiar a ejercicio 5 buscan información
 public class Ejercicio5 {
     public static void main(String[] args) {
         System.out.print("Introduce un parámetro: ");
