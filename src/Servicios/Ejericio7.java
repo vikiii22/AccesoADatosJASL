cefd3d8735a46a17fa448bd421c65b8bbbed668b
JoseASL <joseviki10@gmail.com>
2021-10-07 13:05:11 +0200

    Ejercicio 7 procesos

---
+++ src/Servicios/Ejericio7.java
@@ -0,0 +1,29 @@
+package Servicios;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class Ejericio7 {
+    public static void main(String[] args) {
+        String comando="ipconfig";
+        String linea;
+        Process p=null;
+        InputStream inputStream;
+        BufferedReader bufferedReader;
+
+        try {
+            p=Runtime.getRuntime().exec(comando);
+            inputStream=p.getInputStream();
+            bufferedReader=new BufferedReader(new InputStreamReader(inputStream));
+            while ((linea = bufferedReader.readLine()) != null){
+                if (linea.contains("IPv4")){
+                    System.out.println(linea);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
