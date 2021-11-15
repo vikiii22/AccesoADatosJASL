d75b1cac6301f5a45c4265b3ef79c04b2e05ba66
JoseASL <joseviki10@gmail.com>
2021-11-15 22:22:38 +0100

    Ejercicios PSP

--- src/PSP/ThreadsIII/Ejercicios/Orden_JoseASanchezLopez.java
+++ src/PSP/ThreadsIII/Ejercicios/Orden_JoseASanchezLopez.java
@@ -8,7 +8,7 @@ public class Orden_JoseASanchezLopez extends Thread {
 
     public Orden_JoseASanchezLopez(String texto, Semaphore s) {
         this.texto = texto;
-        this.s=s;
+        this.s = s;
     }
 
     @Override
@@ -24,13 +24,13 @@ public class Orden_JoseASanchezLopez extends Thread {
     }
 }
 
-class Orden2 extends Thread{
+class Orden2 extends Thread {
     String texto;
     Semaphore s;
 
     public Orden2(String texto, Semaphore s) {
         this.texto = texto;
-        this.s=s;
+        this.s = s;
     }
 
     @Override
@@ -47,16 +47,32 @@ class Orden2 extends Thread{
     }
 }
 
+class Contador {
+    int contador = 0;
+
+    public Contador(int contador) {
+        this.contador = contador;
+    }
+
+    public synchronized void incremento() {
+        contador++;
+    }
+
+    public synchronized int value() {
+        return contador;
+    }
+}
+
 class EjecutaOrden {
     public static void main(String[] args) throws InterruptedException {
-        int contador=0;
-        Semaphore sem=new Semaphore(contador);
+        Contador contador = new Contador(0);
+        Semaphore sem = new Semaphore(contador.value());
 
         Thread orden1 = new Thread(new Orden_JoseASanchezLopez("Probando semáforos 1:", sem));
         Thread orden2 = new Thread(new Orden2("Probando semáforos 2:", sem));
 
-        contador++;
-        if (contador==1){
+        contador.incremento();
+        if (contador.value() == 1) {
             orden2.start();
             sem.release();
         }
