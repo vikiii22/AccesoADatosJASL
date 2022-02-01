fbd41822e832c2fbfbebb0b865471e108f6f2524
JoseASL <joseviki10@gmail.com>
2022-02-01 22:58:53 +0100

    Repaso examen

--- src/PSP/TCP/RepasoExamen/Prueba1/Cliente.java
+++ src/PSP/TCP/RepasoExamen/Prueba1/Cliente.java
@@ -1,4 +1,27 @@
 package PSP.TCP.RepasoExamen.Prueba1;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
 public class Cliente {
+    public static void main(String[] args) {
+        try {
+            Socket socket=new Socket();
+            InetSocketAddress addr=new InetSocketAddress("localhost", 5554);
+
+            socket.connect(addr);
+
+            DataOutputStream dos=new DataOutputStream(socket.getOutputStream());
+            DataInputStream dis=new DataInputStream(socket.getInputStream());
+
+            System.out.println(dis.readUTF());
+            System.out.println("Mensaje recibido");
+            dos.writeUTF("Hola desde el cliente");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/PSP/TCP/RepasoExamen/Prueba1/Servidor.java
+++ src/PSP/TCP/RepasoExamen/Prueba1/Servidor.java
@@ -1,4 +1,33 @@
 package PSP.TCP.RepasoExamen.Prueba1;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Servidor {
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket=new ServerSocket();
+            Socket socket=null;
+            InetSocketAddress addr=new InetSocketAddress("localhost", 5554);
+            serverSocket.bind(addr);
+            int cliente=1;
+            while (cliente <= 3) {
+                cliente++;
+                socket = serverSocket.accept();
+                DataOutputStream dos=new DataOutputStream(socket.getOutputStream());
+                DataInputStream dis=new DataInputStream(socket.getInputStream());
+
+                dos.writeUTF("Hola desde el servidor");
+                System.out.println(dis.readUTF());
+            }
+            socket.close();
+            serverSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
