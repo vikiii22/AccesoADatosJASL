3d7da26eb96de72538867a6d1ed8a69c7deaa679
JoseASL <joseviki10@gmail.com>
2022-02-02 01:21:07 +0100

    Ejercicios PSP

--- src/PSP/TCP/RepasoExamen/Prueba1/Cliente.java
+++ src/PSP/TCP/RepasoExamen/Prueba1/Cliente.java
@@ -1,26 +1,37 @@
 package PSP.TCP.RepasoExamen.Prueba1;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Cliente {
     public static void main(String[] args) {
         try {
-            Socket socket=new Socket();
-            InetSocketAddress addr=new InetSocketAddress("localhost", 5554);
+            while (true) {
+                Socket socket=new Socket();
+                InetSocketAddress addr=new InetSocketAddress("localhost", 5554);
+                Scanner sc=new Scanner(System.in);
 
-            socket.connect(addr);
+                socket.connect(addr);
 
-            DataOutputStream dos=new DataOutputStream(socket.getOutputStream());
-            DataInputStream dis=new DataInputStream(socket.getInputStream());
+                DataOutputStream dos=new DataOutputStream(socket.getOutputStream());
+                DataInputStream dis=new DataInputStream(socket.getInputStream());
 
-            System.out.println(dis.readUTF());
-            System.out.println("Mensaje recibido");
-            dos.writeUTF("Hola desde el cliente");
-        } catch (IOException e) {
+                String archivos=dis.readUTF();
+                System.out.println(archivos);
+                dos.writeUTF("Hola desde el cliente");
+                System.out.println(dis.readUTF());
+                String archivo=sc.nextLine();
+                dos.writeUTF(archivo);
+                String archivosRecibidos=dis.readUTF();
+                System.out.println("PREPARANDO DESCARGA");
+                Thread.sleep(1500);
+                System.out.println("CONTENIDO DESCARGADO");
+                System.out.println(archivosRecibidos);
+                Thread.sleep(1500);
+            }
+        } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
     }
--- src/PSP/TCP/RepasoExamen/Prueba1/Servidor.java
+++ src/PSP/TCP/RepasoExamen/Prueba1/Servidor.java
@@ -1,8 +1,6 @@
 package PSP.TCP.RepasoExamen.Prueba1;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -14,18 +12,32 @@ public class Servidor {
             Socket socket=null;
             InetSocketAddress addr=new InetSocketAddress("localhost", 5554);
             serverSocket.bind(addr);
-            int cliente=1;
-            while (cliente <= 3) {
-                cliente++;
+            while (true) {
                 socket = serverSocket.accept();
                 DataOutputStream dos=new DataOutputStream(socket.getOutputStream());
                 DataInputStream dis=new DataInputStream(socket.getInputStream());
+                String archivos="";
 
-                dos.writeUTF("Hola desde el servidor");
+                //dos.writeUTF("Hola desde el servidor");
+                File file=new File(".");
+                for (File f:file.listFiles()){
+                    if (f.isFile()){
+                        archivos += f.toString()+"\n";
+                    }
+                }
+                dos.writeUTF(archivos);
                 System.out.println(dis.readUTF());
+                dos.writeUTF("Selecciona un archivo: ");
+                String archivo=dis.readUTF();
+
+                BufferedReader br=new BufferedReader(new FileReader(archivo));
+                String contenido="";
+                String contenidos="";
+                while ((contenido=br.readLine())!=null){
+                    contenidos += contenido+"\n";
+                }
+                dos.writeUTF(contenidos);
             }
-            socket.close();
-            serverSocket.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
