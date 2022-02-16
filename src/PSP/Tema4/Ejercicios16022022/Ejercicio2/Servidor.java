fb05a972ec627d9b482466af130ad8297a5ef5e6
JoseASL <joseviki10@gmail.com>
2022-02-16 14:21:30 +0100

    Ejercicios PSP

--- src/PSP/Tema4/Ejercicios16022022/Ejercicio2/Servidor.java
+++ src/PSP/Tema4/Ejercicios16022022/Ejercicio2/Servidor.java
@@ -10,20 +10,17 @@ public class Servidor {
     public static void main(String[] args) throws IOException {
         DatagramSocket datagram = null;
 
-        datagram=new DatagramSocket(2541);
-        while (true){
-            byte[] entrada=new byte[5];
-            DatagramPacket packet=new DatagramPacket(entrada, 5);
+        datagram = new DatagramSocket(2541);
+        while (true) {
+            byte[] entrada = new byte[5];
+            DatagramPacket packet = new DatagramPacket(entrada, 5);
             datagram.receive(packet);
-            String recibe=new String(entrada);
-            if (recibe.equalsIgnoreCase("s")){
-                String salida=String.valueOf(packet.getAddress());
-                byte[] salida1=salida.getBytes();
-                int puerto=packet.getPort();
-                InetAddress address=packet.getAddress();
-                packet=new DatagramPacket(salida1, salida1.length, address, puerto);
-                datagram.send(packet);
-            }
+            String salida = String.valueOf(packet.getAddress());
+            byte[] salida1 = salida.getBytes();
+            int puerto = packet.getPort();
+            InetAddress address = packet.getAddress();
+            packet = new DatagramPacket(salida1, salida1.length, address, puerto);
+            datagram.send(packet);
         }
     }
 }
